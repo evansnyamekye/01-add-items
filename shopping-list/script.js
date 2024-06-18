ae30a31301e6eaffade59e00447d2d8daf4cb0fe
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-06-18 03:39:09 +0000

    Update: add remove and clear functions to the shopping listy app

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,7 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear")
 
 function addItem(e) {
   e.preventDefault();
@@ -8,38 +9,52 @@ function addItem(e) {
   const newItem = itemInput.value;
 
   // Validate Input
-  if (newItem === '') {
-    alert('Please add an item');
+  if (newItem === "") {
+    alert("Please add an item");
     return;
   }
 
   // Create list item
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
 
-  const button = createButton('remove-item btn-link text-red');
+  const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
   itemList.appendChild(li);
 
-  itemInput.value = '';
+  itemInput.value = "";
 }
 
 function createButton(classes) {
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.className = classes;
-  const icon = createIcon('fa-solid fa-xmark');
+  const icon = createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
   return button;
 }
 
 function createIcon(classes) {
-  const icon = document.createElement('i');
+  const icon = document.createElement("i");
   icon.className = classes;
   return icon;
 }
 
-// Event Listeners
-itemForm.addEventListener('submit', addItem);
+function removeItem(e){
+  if (e.target.parentElement.classList.contains('removeItem')) {
+    e.target.parentElement.parentElement.remove(); 
+  }
+}
 
-//this is the final code fot the list itme 
+function clearBtn() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild); 
+  }
+}
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener('click', clearItems);
+
+//this is the final code fot the list itme
