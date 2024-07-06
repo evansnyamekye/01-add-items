8e800fb6d34de52ead0eb129448b151c1e645045
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-07-06 23:54:22 +0000

    Add files

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,54 +1,80 @@
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-list");
-const clearBtn = document.getElementById("clear");
-const itemFilter = document.getElementById("filter");
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
 
   // Validate Input
-  if (newItem === "") {
-    alert("Please add an item");
+  if (newItem === '') {
+    alert('Please add an item');
     return;
-  }
+  } 
 
+  // Create item DOM element
+  addItemToDOM(newItem);
+
+  // Add item to local storage
+  addItemToStorage(newItem);
+
+  checkUI();
+
+  itemInput.value = '';
+}
+
+function addItemToDOM(item) {
   // Create list item
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
 
-  const button = createButton("remove-item btn-link text-red");
+  const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
-
-  checkUI(); 
-
-  itemInput.value = "";
-
-  newItem = "";
 }
 
 function createButton(classes) {
-  const button = document.createElement("button");
+  const button = document.createElement('button');
   button.className = classes;
-  const icon = createIcon("fa-solid fa-xmark");
+  const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 }
 
 function createIcon(classes) {
-  const icon = document.createElement("i");
+  const icon = document.createElement('i');
   icon.className = classes;
   return icon;
 }
 
+function addItemToStorage(item) {
+  let itemsFromStorage;
+
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  // Add new item to array
+  itemsFromStorage.push(item);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
 function removeItem(e) {
-  if (e.target.parentElement.classList.contains("remove-item")) {
-    if (confirm('Are you sure'))
-    e.target.parentElement.parentElement.remove();
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
   }
 }
 
@@ -56,23 +82,41 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
 
 function checkUI() {
-  const items = itemList.querySelectorAll("li");
+  const items = itemList.querySelectorAll('li');
+
   if (items.length === 0) {
-    clearBtn.style.display = "none";
-    itemFilter.style.display = "none";
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
   } else {
-    clearBtn.style.display = "block";
-    itemFilter.style.display = "block";
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 }
 
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
-itemList.addEventListener("click", removeItem);
-clearBtn.addEventListener("click", clearItems);
+itemForm.addEventListener('submit', onAddItemSubmit);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
 
-//check User interface
 checkUI();
