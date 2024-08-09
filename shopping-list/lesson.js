33edd8ec7d39103f875f6f9fb7882ace6b4f7d0e
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-08-09 11:56:08 +0000

    Update: added removeItem and clearItem in the shopping list

---
+++ shopping-list/lesson.js
@@ -0,0 +1,58 @@
+//Declaration of variables
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById('clear')
+
+//validation/form prompt
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  if (newItem === "") {
+    alert("Please add an item");
+    return;
+  }
+
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button); 
+
+  itemList.appendChild(li); 
+
+  itemInput.value = '';
+}
+
+function createButton(classes){
+    const button = document.createElement('button')
+    button.className = classes; 
+    const icon = createIcon ('fa-solid fa-xmark')
+    button.appendChild(icon); 
+    return button; 
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i'); 
+    icon.className = classes; 
+    return icon; 
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.parentElement.contains('remove-item')); 
+    e.target.parentElement.parentElement.remove(); 
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild )
+  }
+}
+
+//add event listenr
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener('click', removeItem); 
+clearBtn.addEventListener('click', clearItems); 
+
